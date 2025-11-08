ab44669f85638e33856851eb5218730577f507b0
Simone Salucci <simone.salucci3@studio,unibo.it>
2025-11-08 18:01:47 +0100

    Exercise 71 part 1

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,65 @@
+package it.unibo.inner.impl;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+    private final T[] elements;
+    private Predicate<T> predicate;
+
+    public IterableWithPolicyImpl(T[] elements){
+        this(elements, new Predicate<T>() {
+            @Override
+            public boolean test(T elem) {
+                return true;
+            }
+            
+        });
+    }
+
+    public IterableWithPolicyImpl(T[] elements, Predicate<T> filter){
+        this.elements = elements;
+        setIterationPolicy(filter);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new PolicyIterator();
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        this.predicate = filter;
+    }
+
+
+    private class PolicyIterator implements Iterator<T>{
+        private int current;
+
+        private PolicyIterator(){
+            current = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            if (current >= elements.length) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T n = elements[current];
+            current++;
+            return n;
+        }
+        
+    }
+    
+}
